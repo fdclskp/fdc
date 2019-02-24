6c84d6d09e6d1b7417d041908ddfb7679f8fa7ca
kikuchi shoma <kicchii@kikuchi-no-MacBook-Pro.local>
2019-02-24 21:00:40 +0900

    update

--- kicchi/work/GraphConv/utils.py
+++ kicchi/work/GraphConv/utils.py
@@ -28,7 +28,6 @@ def array_convert(filename):
 	#後で一つにまとめる(array_y)
 	graph_array = []
 	node_list = []
-	date_list = []
 	g = Graph()
 	for line in open(filename, 'r'):
 		if line == '\n':
@@ -37,13 +36,12 @@ def array_convert(filename):
 			g = Graph()
 			node_list = []
 		elif line[4] == '-':
-			date_list.append(line[4])
+			continue
 		else:
 			line = line[:-1].split(' ')
 			line = [int(s) for s in line]
 			node_list.append(line)
-		
-	return graph_array, date_list
+	return graph_array
 
 def array_y(filename):
 	y_data = []
@@ -54,7 +52,6 @@ def array_y(filename):
 			y = []
 		else:
 			line = line[:-1]
-			
 			y.append(float(line))
 
 	return y_data
